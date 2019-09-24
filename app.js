cb77bd30bb3ad5b0253a450e7659a9136ad56a20
Victor Thoma <victor@mrundmrsthoma.de>
2019-09-24 20:10:07 +0200

    basic signup flow mit passport. Session not working

--- app.js
+++ app.js
@@ -1,26 +1,51 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require("http-errors");
+let express = require("express");
+let path = require("path");
+let cookieParser = require("cookie-parser");
+let logger = require("morgan");
+const mongoose = require("mongoose");
+mongoose.connect(`mongodb://localhost/metadomain`);
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const MongoStore = require("connect-mongo")(session);
 
-var app = express();
+let indexRouter = require("./routes/index");
+let usersRouter = require("./routes/users");
+let authRouter = require("./routes/auth");
+
+let app = express();
+
+app.use(
+  session({
+    secret: "abc", // does not matter at all
+    store: new MongoStore({
+      // this is going to create the `sessions` collection in the db
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    })
+  })
+);
+
+require("./config/passport.js");
+app.use(passport.initialize());
+app.use(passport.session());
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/", authRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,11 +56,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
