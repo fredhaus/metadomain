805e27a22916c4c775ff52ff8aaa3ce5e691bc30
Fred Haus <fredhaus@Freds-MBP-235.fritz.box>
2019-09-27 22:31:47 +0200

    handling various faulty nameCom api response cases, added available: true/false check for all api calls (also to responseObj of api call functions)

--- api_calls.js
+++ api_calls.js
@@ -1,12 +1,7 @@
-// let secrets = require("./api_secrets/api_secrets");
-// console.log(secrets.nameCom);
-
-//___________
-
-
-
 var axios = require("axios");
 const Epik = require("./node_modules/epik.com");
+
+// Gandi _________________________________________________
 
 let get_gandi_data = userInput => {
   const headers = {
@@ -19,21 +14,31 @@ let get_gandi_data = userInput => {
   };
 
   return axios.request(options).then(response => {
-    // console.log("GANDI______________________")
-    // console.log(response.data);
-    // console.log("______________________")
-    let responseObj = {
-      name: "gandi",
-      data: response.data, 
-      price: response.data.products[0].prices[0].price_after_taxes
-    };
+    let responseObj = {};
 
+    if (response.data.products[0].status === "available") {
+      responseObj = {
+        name: "gandi",
+        data: response.data,
+        price: response.data.products[0].prices[0].price_after_taxes,
+        available: true
+      };
+    } else {
+      responseObj = {
+        name: "gandi // not available",
+        data: response.data,
+        price: 0,
+        available: false
+      };
+    }
     return responseObj;
   });
 };
 
+// nameCom _________________________________________________
+
 let get_nameCom_data = userInput => {
-  var dataString = { keyword: "thedopestdomainever.com" };
+  var dataString = { keyword: userInput };
 
   var options = {
     url: "https://api.dev.name.com/v4/domains:search",
@@ -45,65 +50,86 @@ let get_nameCom_data = userInput => {
     }
   };
 
-  return axios.request(options).then(response => {
-    // console.log("nameCom______________________")
-    // console.log(response.data);
-    // console.log("______________________")
-    let responseObj = {
-      name: "nameCom",
-      data: response.data,
-      price: response.data.results[0].purchasePrice
-    };
+  let ctr = 0;
 
-    return responseObj;
-  });
+  let axiosCall = () => {
+    return axios.request(options).then(response => {
+      if (ctr < 3) {
+        // try three times, in case of faulty API response
+        ctr++;
+        if (
+          Object.keys(response.data).length !== 0 && //  >>>>>>> not empty
+          response.data.results[0].domainName === userInput &&
+          response.data.results[0].purchasable === true
+        ) {
+          // console.log(response.data.results);
+          let responseObj = {
+            name: "nameCom",
+            data: response.data,
+            price: response.data.results[0].purchasePrice,
+            available: true
+          };
+          return responseObj;
+        } else {
+          return axiosCall();
+        }
+      } else if (
+        Object.keys(response.data).length !== 0 && //  >>>>>>> not empty
+        response.data.results[0].domainName === userInput &&
+        !response.data.results[0].purchasable
+      ) {
+        return {
+          name: "NameCom // not available",
+          price: 0.0,
+          data: { price: "0.00", currency: "EUR" },
+          available: false
+        };
+      } else {
+        return {
+          name: "NameCom // Unresponsive",
+          price: 0.0,
+          data: { price: "0.00", currency: "EUR" },
+          available: false
+        };
+      }
+    });
+  };
+  return axiosCall();
 };
 
+// EPIK _________________________________________________
+
 let get_epik_data = async userInput => {
-  //2. Initiate the Epik API Client with your API Signature
   const EpikClient = new Epik("44F0-B9DB-DEAA-8200");
 
   return EpikClient.domains.checkAvailability(userInput).then(response => {
-    // console.log("Epik______________________")
-    // console.log(response.data);
-    // console.log("______________________")
-    let responseObj = {
-      name: "epik",
-      data: response.data,
-      price: response.data.details.domain.price
-    };
+    console.log(response.data);
+
+    let responseObj = {};
+
+    if (response.data.details.domain.available === true) {
+      responseObj = {
+        name: "epik",
+        data: response.data,
+        price: response.data.details.domain.price,
+        available: true
+      };
+    } else {
+      responseObj = {
+        name: "epik // not available",
+        data: response.data,
+        price: 0,
+        available: false
+      };
+    }
     return responseObj;
   });
 };
+
+// _________________________________________________
 
 module.exports = {
   get_nameCom_data,
   get_gandi_data,
   get_epik_data
 };
-
-// module.exports = get_epik_data
-
-// testing_________________
-
-// console.log("TEST geht los");
-// get_nameCom_data("mooooooooooooin.com");
-// get_gandi_data("mooooooooooooin.com");
-// get_epik_data("mooooooooooooin.com");
-
-//______ Hendik TA Advice, how to execute in different file
-
-// get_epik_data("mooooooooooooin.com").then(response){
-//   response.//do something
-// }
-
-// let resultArrAll =[
-//   get_epik_data("mooooooooooooin.com"),get_epik_data("mooooooooooooin.com"),get_epik_data("mooooooooooooin.com")
-// ]
-
-// Promise.all(resultArrAll).then(results => {
-//   // results = Array mit den Ergebnissen, in der gleichen Reihenfolge wie Promsises reingeben.
-//   resukts.sort((a, b) => {
-
-//   })
-// });
